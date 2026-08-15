622bfbd2ff51753912d8c6cd00b559fc6f6a1a4d
Yajat Charpe <46809043+Y9C4@users.noreply.github.com>
2026-08-15 21:04:10 +1000

    CML tests

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { Separator } from "@/components/ui/separator"
 
 export default function Dashboard() {
   return (
+    <div>
     <div className="flex w-full h-full items-start justify-center bg-card p-8">
       <div className="absolute top-4 right-4">
         <ThemeToggle />
@@ -13,6 +14,17 @@ export default function Dashboard() {
         <TickerHistory />
         <Separator />
         <Sp500BackfillButton />
+      </div>
+      
+    </div>
+      <div className="flex w-full h-full justify-center max-w-3xl flex-col gap-6 bg-secondary">
+        <h1 className="text-xl text-foreground">
+          Portfolio Builder
+        </h1>
+        <h3 classname="text-sm text-foreground/50">
+          Build an efficient portfolio from the 500 stocks on the S&P500 with a risk free rate derrived as the annualised average of the US 13 week treasury bonds. The efficient portfolio is derrived using a convex optimization via CVXPY using Ledoit-Wolf shrinkage on the VarCov matrix. 
+        </h3>
+
       </div>
     </div>
   )
